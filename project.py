902539117abdda2b5d850e9f1ae45095f10e5e92
Mina-C <mina.choe@outlook.com>
2019-08-07 20:21:24 +0200

    feat: create functions adding user info to db

--- project.py
+++ project.py
@@ -318,6 +318,27 @@ def deleteMenuItem(restaurant_id, menu_id):
         return render_template('deleteMenuItem.html', item=itemToDelete)
 
 
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email = email).one()
+        return user.id
+    except:
+        return None
+
+
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id = user_id).one()
+    return user
+
+
+def createUser(login_session):
+    newUser = User(name = login_session['username'], email= login_session['email'], picture = login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email = login_session['email']).one()
+    return user.id
+
+
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
