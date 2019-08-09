8afeed19d59dbdd4fe368025d3be5662e558a97f
Mina-C <mina.choe@outlook.com>
2019-08-09 20:20:31 +0200

    feat: add facebook disconnect function

--- project.py
+++ project.py
@@ -115,6 +115,18 @@ def fbconnect():
     return output
 
 
+# depopulate the login session
+@app.route('/fbdisconnect')
+def fbdisconnect():
+    facebook_id = login_session['facebook_id']
+    # The access token must me included to successfully logout
+    access_token = login_session['access_token']
+    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id,access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'DELETE')[1]
+    return "you have been logged out"
+
+
 # server-side function
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
